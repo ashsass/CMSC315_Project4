bce33b29335e8e25abb46da880d6857ce8e2248a
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2024-03-05 11:20:45 -0500

    Modified the code from Pearson book for dfs. Could use some cleaning.

--- CMSC315_Project4/src/application/UndirectedGraph.java
+++ CMSC315_Project4/src/application/UndirectedGraph.java
@@ -22,10 +22,41 @@ import java.util.*;
 
 public class UndirectedGraph {
 	private int nameIndex = 0; //needed to put this here to create a baseline value for the name - once it is called once the first name should be A
-	private int vertexListIndex = 0; //need to be able to put the point in the correct place in the vertex list - this is getting complicated now that it is a list holding an arraylist 
+	private static int vertexListIndex = 0; //need to be able to put the point in the correct place in the vertex list - this is getting complicated now that it is a list holding an arraylist 
+	
+	public static void main(String[] args) {
+		vertexList.add(new ArrayList<>());
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "A"));
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "F"));
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "E"));
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "B"));
+		vertexListIndex++;
+		vertexList.add(new ArrayList<>());
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "B"));
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "A"));
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "D"));
+		vertexListIndex++;
+		vertexList.add(new ArrayList<>());
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "C"));
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "D"));
+		vertexListIndex++;
+		vertexList.add(new ArrayList<>());
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "D"));
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "B"));
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "C"));
+		vertexListIndex++;
+		vertexList.add(new ArrayList<>());
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "E"));
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "A"));
+		vertexListIndex++;
+		vertexList.add(new ArrayList<>());
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "F"));
+		vertexList.get(vertexListIndex).add(new Point(0.0, 0.0, "A"));
+		dfs();
+	}
 
 	/* the index indicates the name of the point */
-	private List<ArrayList<Point>> vertexList = new ArrayList<>();
+	private static List<ArrayList<Point>> vertexList = new ArrayList<>();
 	
 	/* no-constructor */
 	public UndirectedGraph() {
@@ -67,4 +98,35 @@ public class UndirectedGraph {
 		vertexList.get(i2).add(p1);
 		
 	}
+	
+	public static List<String> dfs() {
+		int start = 0; //Start at A
+	    List<String> searchOrder = new ArrayList<>();
+	    int[] parent = new int[vertexList.size()];
+	    for (int i = 0; i < parent.length; i++)
+	    		parent[i] = -1; 
+
+	    // Mark visited vertices
+	    boolean[] isVisited = new boolean[vertexList.size()];
+
+	    // Recursively search
+	    dfs(start, parent, searchOrder, isVisited);
+
+	    return searchOrder;
+	  }
+
+	  /** Recursive method for DFS search */
+	  private static void dfs(int i, int[] parent, List<String> searchOrder,
+	      boolean[] isVisited) {
+		  searchOrder.add(vertexList.get(i).get(0).getName());
+		  isVisited[i] = true; 
+		  
+		  for (Point e : vertexList.get(i)) { 
+			  int next = (int)e.getName().charAt(0) - 'A';
+			  if (!isVisited[next]) { 
+				  parent[next] = i; 
+				  dfs(next, parent, searchOrder, isVisited); 
+		      }
+	    }
+	  }
 }
