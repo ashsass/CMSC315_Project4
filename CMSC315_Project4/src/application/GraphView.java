a13c330bbb8e5123451bff36572a65353d0cb501
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2024-03-05 14:39:36 -0500

    Cleaned up code.

--- CMSC315_Project4/src/application/GraphView.java
+++ CMSC315_Project4/src/application/GraphView.java
@@ -33,8 +33,6 @@ public class GraphView extends BorderPane {
 						//create a new point and turn it into a circle
 						Point newPoint = graph.addVertex(x, y);
 						drawVertex(newPoint);
-//						getChildren().addAll(new Circle(x, y, 5.0),
-//								new Text(x - 5, y - 10, newPoint.getName()));
 					}
 				}
 		});
@@ -45,17 +43,10 @@ public class GraphView extends BorderPane {
 				new Text(point.getX() - 5, point.getY() - 10, point.getName()));
 	}
 	 
-	 public void drawEdge(String v1, String v2) throws IndexOutOfBoundsException{
-		 //If in lower case change to upper case 
-		 
-		 //Validate that the vertices exist
-		 	//IndexOutOfBoundsException("One of the vertices not found");
-		 
-		 
+	 public void drawEdge(String v1, String v2) {
 		 if(v1.length() == 1 && v2.length() == 1) {
 			 int index1 = (int)v1.charAt(0) - 'A';
 			 int index2 = (int)v2.charAt(0) - 'A';
-		//Will need to validate points before getting here - come back to it
 			 Point p1 = graph.getPoint(index1);
 			 Point p2 = graph.getPoint(index2);
 			 graph.addEdge(index1, p1, index2, p2);
